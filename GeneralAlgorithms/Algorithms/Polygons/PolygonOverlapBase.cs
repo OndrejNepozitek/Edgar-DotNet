243770405eee89bebac6d86e9f944aebc66fb5f9
OndrejNepozitek <ondra@nepozitek.cz>
2018-03-09 21:33:07 +0100

    Improve DoTouch function of PolygonOverlapBase

--- GeneralAlgorithms/Algorithms/Polygons/PolygonOverlapBase.cs
+++ GeneralAlgorithms/Algorithms/Polygons/PolygonOverlapBase.cs
@@ -73,6 +73,14 @@
 			if (minimumLength < 0)
 				throw new ArgumentException("The minimum length must by at least 0.", nameof(minimumLength));
 
+			var bounding1 = GetBoundingRectangle(polygon1) + position1;
+			var bounding2 = GetBoundingRectangle(polygon2) + position2;
+
+			if (!DoOverlap(bounding1, bounding2) && !DoTouch(bounding1, bounding2, minimumLength))
+			{
+				return false;
+			}
+
 			var decomposition1 = GetDecomposition(polygon1).Select(x => x + position1);
 			var decomposition2 = GetDecomposition(polygon2).Select(x => x + position2);
 
@@ -80,14 +88,24 @@
 			{
 				foreach (var r2 in decomposition2)
 				{
-					var overlapX = Math.Max(-1, Math.Min(r1.B.X, r2.B.X) - Math.Max(r1.A.X, r2.A.X));
-					var overlapY = Math.Max(-1, Math.Min(r1.B.Y, r2.B.Y) - Math.Max(r1.A.Y, r2.A.Y));
-
-					if ((overlapX == 0 && overlapY >= minimumLength) || (overlapY == 0 && overlapX >= minimumLength))
+					if (DoTouch(r1, r2, minimumLength))
 					{
 						return true;
 					}
 				}
+			}
+
+			return false;
+		}
+
+		protected bool DoTouch(GridRectangle rectangle1, GridRectangle rectangle2, int minimumLength)
+		{
+			var overlapX = Math.Max(-1, Math.Min(rectangle1.B.X, rectangle2.B.X) - Math.Max(rectangle1.A.X, rectangle2.A.X));
+			var overlapY = Math.Max(-1, Math.Min(rectangle1.B.Y, rectangle2.B.Y) - Math.Max(rectangle1.A.Y, rectangle2.A.Y));
+
+			if ((overlapX == 0 && overlapY >= minimumLength) || (overlapY == 0 && overlapX >= minimumLength))
+			{
+				return true;
 			}
 
 			return false;
