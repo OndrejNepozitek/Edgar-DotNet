0b9151829212f2dcf98b7160426f449cade583b6
OndrejNepozitek <ondra@nepozitek.cz>
2018-05-06 17:59:15 +0200

    Refactor the MapGeneration.Interfaces project

--- MapGeneration.Interfaces/Utils/ISerializer.cs
+++ MapGeneration.Interfaces/Utils/ISerializer.cs
@@ -2,9 +2,12 @@
 {
 	using System.Collections.Generic;
 	using System.IO;
-	using Core;
 	using Core.MapLayouts;
 
+	/// <summary>
+	/// Represents types that are able to serialize IMapLayout.
+	/// </summary>
+	/// <typeparam name="TNode"></typeparam>
 	public interface ISerializer<TNode>
 	{
 		void Serialize(IMapLayout<TNode> layout, StreamWriter writer);
