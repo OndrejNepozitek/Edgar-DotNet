fcbbd5ee848280ce28464537caac2f9f304b81ed
OndrejNepozitek <ondra@nepozitek.cz>
2018-12-30 21:47:55 +0100

    Merge door lines before generating configuration spaces. In some cases, doing so makes the computation faster.

---
+++ MapGeneration.Tests/Core/Doors/DoorUtilsTests.cs
@@ -0,0 +1,37 @@
+﻿namespace MapGeneration.Tests.Core.Doors
+{
+	using System.Collections.Generic;
+	using GeneralAlgorithms.DataStructures.Common;
+	using Interfaces.Core.Doors;
+	using MapGeneration.Core.Doors;
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class DoorUtilsTests
+	{
+		[Test]
+		public void MergeDoorLines_CorrectlyMerges()
+		{
+			var doorLines = new List<IDoorLine>()
+			{
+				new DoorLine(new OrthogonalLine(new IntVector2(1, 0), new IntVector2(2, 0)), 2),
+				new DoorLine(new OrthogonalLine(new IntVector2(3, 0), new IntVector2(5, 0)), 2),
+				new DoorLine(new OrthogonalLine(new IntVector2(-3, 0), new IntVector2(0, 0)), 1),
+				new DoorLine(new OrthogonalLine(new IntVector2(-3, 0), new IntVector2(0, 0)), 2),
+				new DoorLine(new OrthogonalLine(new IntVector2(0, 0), new IntVector2(0, 3)), 2),
+				new DoorLine(new OrthogonalLine(new IntVector2(0, -2), new IntVector2(0, -1)), 2),
+			};
+
+			var expectedDoorLines = new List<IDoorLine>()
+			{
+				new DoorLine(new OrthogonalLine(new IntVector2(-3, 0), new IntVector2(0, 0)), 1),
+				new DoorLine(new OrthogonalLine(new IntVector2(-3, 0), new IntVector2(5, 0)), 2),
+				new DoorLine(new OrthogonalLine(new IntVector2(0, -2), new IntVector2(0, 3)), 2),
+			};
+
+			var mergedDoorLines = DoorUtils.MergeDoorLines(doorLines);
+
+			Assert.That(mergedDoorLines, Is.EquivalentTo(expectedDoorLines));
+		}
+	}
+}
