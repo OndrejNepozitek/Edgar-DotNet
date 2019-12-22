635c917b2727119aadad6398c128d9a1e967d484
Ondřej Nepožitek <ondra@nepozitek.cz>
2019-12-22 11:46:50 +0100

    Add map description that maps from generic TNode to ints

---
+++ MapGeneration.IntegrationTests/Core/MapDescriptions/MapDescriptionMappingTests.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using GeneralAlgorithms.DataStructures.Polygons;
+using MapGeneration.Core.Doors.DoorModes;
+using MapGeneration.Core.MapDescriptions;
+using MapGeneration.Interfaces.Core.MapDescriptions;
+using NUnit.Framework;
+
+namespace MapGeneration.IntegrationTests.Core.MapDescriptions
+{
+    [TestFixture]
+    public class MapDescriptionMappingTests
+    {
+        [Test]
+        public void BasicTest()
+        {
+            var roomTemplate1 = new RoomTemplate(GridPolygon.GetSquare(10), new OverlapMode(1, 0));
+            var roomTemplate2 = new RoomTemplate(GridPolygon.GetRectangle(5, 10), new OverlapMode(1, 0));
+
+            var roomDescription1 = new BasicRoomDescription(new List<IRoomTemplate>() { roomTemplate1 });
+            var roomDescription2 = new BasicRoomDescription(new List<IRoomTemplate>() { roomTemplate2 });
+
+            var mapDescription = new MapDescription<string>();
+            mapDescription.AddRoom("0", roomDescription1);
+            mapDescription.AddRoom("1", roomDescription2);
+            mapDescription.AddConnection("0", "1");
+
+            var mapDescriptionMapping = new MapDescriptionMapping<string>(mapDescription);
+            var mapping = mapDescriptionMapping.GetMapping();
+
+            Assert.That(mapDescriptionMapping.GetRoomDescription(mapping["0"]), Is.EqualTo(roomDescription1));
+            Assert.That(mapDescriptionMapping.GetRoomDescription(mapping["1"]), Is.EqualTo(roomDescription2));
+            Assert.That(mapDescriptionMapping.GetGraph().VerticesCount, Is.EqualTo(2));
+            Assert.That(mapDescriptionMapping.GetGraph().HasEdge(mapping["0"], mapping["1"]), Is.True);
+        }
+
+        [Test]
+        public void BasicCorridorsTest()
+        {
+            var roomTemplate1 = new RoomTemplate(GridPolygon.GetSquare(10), new OverlapMode(1, 0));
+            var roomTemplate2 = new RoomTemplate(GridPolygon.GetRectangle(5, 10), new OverlapMode(1, 0));
+
+            var roomDescription1 = new BasicRoomDescription(new List<IRoomTemplate>() { roomTemplate1 });
+            var roomDescription2 = new BasicRoomDescription(new List<IRoomTemplate>() { roomTemplate2 });
+            var corridorDescription = new CorridorRoomDescription(new List<IRoomTemplate>() { roomTemplate2 });
+
+            var mapDescription = new MapDescription<string>();
+            mapDescription.AddRoom("0", roomDescription1);
+            mapDescription.AddRoom("1", corridorDescription);
+            mapDescription.AddRoom("2", roomDescription2);
+            mapDescription.AddConnection("0", "1");
+            mapDescription.AddConnection("1", "2");
+
+            var mapDescriptionMapping = new MapDescriptionMapping<string>(mapDescription);
+            var mapping = mapDescriptionMapping.GetMapping();
+
+            Assert.That(mapDescriptionMapping.GetRoomDescription(mapping["0"]), Is.EqualTo(roomDescription1));
+            Assert.That(mapDescriptionMapping.GetRoomDescription(mapping["1"]), Is.EqualTo(corridorDescription));
+            Assert.That(mapDescriptionMapping.GetRoomDescription(mapping["2"]), Is.EqualTo(roomDescription2));
+
+            Assert.That(mapDescriptionMapping.GetGraph().VerticesCount, Is.EqualTo(3));
+            Assert.That(mapDescriptionMapping.GetStageOneGraph().VerticesCount, Is.EqualTo(2));
+            Assert.That(mapDescriptionMapping.GetGraph().HasEdge(mapping["0"], mapping["1"]), Is.True);
+            Assert.That(mapDescriptionMapping.GetGraph().HasEdge(mapping["1"], mapping["2"]), Is.True);
+            Assert.That(mapDescriptionMapping.GetStageOneGraph().HasEdge(mapping["0"], mapping["2"]), Is.True);
+        }
+    }
+}
---
+++ MapGeneration/Core/MapDescriptions/MapDescriptionMapping.cs
@@ -0,0 +1,76 @@
+﻿using GeneralAlgorithms.DataStructures.Common;
+using GeneralAlgorithms.DataStructures.Graphs;
+using MapGeneration.Interfaces.Core.MapDescriptions;
+
+namespace MapGeneration.Core.MapDescriptions
+{
+    public class MapDescriptionMapping<TNode> : IMapDescription<int>
+    {
+        private readonly MapDescription<TNode> mapDescription;
+        private readonly TwoWayDictionary<TNode, int> nodeToIntMapping = new TwoWayDictionary<TNode, int>();
+        private readonly IRoomDescription[] roomDescriptions;
+        private readonly IGraph<int> mappedGraph = new UndirectedAdjacencyListGraph<int>();
+        private readonly IGraph<int> mappedStageOneGraph = new UndirectedAdjacencyListGraph<int>();
+
+        public MapDescriptionMapping(MapDescription<TNode> mapDescription)
+        {
+            this.mapDescription = mapDescription;
+            roomDescriptions = new IRoomDescription[mapDescription.GetGraph().VerticesCount];
+            DoMapping();
+        }
+
+        private void DoMapping()
+        {
+            var graph = mapDescription.GetGraph();
+            var stageOneGraph = mapDescription.GetStageOneGraph();
+
+            foreach (var vertex in graph.Vertices)
+            {
+                // Create vertices mapping
+                nodeToIntMapping.Add(vertex, nodeToIntMapping.Count);
+                mappedGraph.AddVertex(nodeToIntMapping[vertex]);
+
+                // Store room description
+                roomDescriptions[nodeToIntMapping[vertex]] = mapDescription.GetRoomDescription(vertex);
+            }
+
+            // Handle main graph edges
+            foreach (var edge in graph.Edges)
+            {
+                mappedGraph.AddEdge(nodeToIntMapping[edge.From], nodeToIntMapping[edge.To]);
+            }
+
+            // Handle stage one graph vertices
+            foreach (var vertex in stageOneGraph.Vertices)
+            {
+                mappedStageOneGraph.AddVertex(nodeToIntMapping[vertex]);
+            }
+
+            // Handle stage one graph edges
+            foreach (var edge in stageOneGraph.Edges)
+            {
+                mappedStageOneGraph.AddEdge(nodeToIntMapping[edge.From], nodeToIntMapping[edge.To]);
+            }
+        }
+
+        public IGraph<int> GetGraph()
+        {
+            return mappedGraph;
+        }
+
+        public IGraph<int> GetStageOneGraph()
+        {
+            return mappedStageOneGraph;
+        }
+
+        public IRoomDescription GetRoomDescription(int node)
+        {
+            return roomDescriptions[node];
+        }
+
+        public TwoWayDictionary<TNode, int> GetMapping()
+        {
+            return nodeToIntMapping;
+        }
+    }
+}
