897577730d44c1d3fb3ae24a20602daa84c05ff1
Ondřej Nepožitek <ondra@nepozitek.cz>
2020-08-13 09:14:42 +0200

    Fix build

--- Edgar/GraphBasedGenerator/Grid2D/RoomTemplateGrid2D.cs
+++ Edgar/GraphBasedGenerator/Grid2D/RoomTemplateGrid2D.cs
@@ -1,7 +1,30 @@
-﻿namespace Edgar.GraphBasedGenerator.Grid2D
+﻿using System.Collections.Generic;
+using Edgar.Legacy.Core.Doors.Interfaces;
+using Edgar.Legacy.Core.MapDescriptions;
+using Edgar.Legacy.GeneralAlgorithms.DataStructures.Common;
+using Edgar.Legacy.GeneralAlgorithms.DataStructures.Polygons;
+
+namespace Edgar.GraphBasedGenerator.Grid2D
 {
     public class RoomTemplateGrid2D
     {
-        
+        public PolygonGrid2D Outline { get; }
+
+        public IDoorMode DoorMode { get; }
+
+        public string Name { get; }
+
+        public RepeatMode? RepeatMode { get; }
+
+        public List<Transformation> AllowedTransformations { get; }
+
+        public RoomTemplateGrid2D(PolygonGrid2D outline, IDoorMode doorMode, string name = null, RepeatMode? repeatMode = null, List<Transformation> allowedTransformations = null)
+        {
+            Outline = outline;
+            DoorMode = doorMode;
+            Name = name ?? "Room template";
+            RepeatMode = repeatMode;
+            AllowedTransformations = allowedTransformations ?? new List<Transformation>() { Transformation.Identity };;
+        }
     }
 }
