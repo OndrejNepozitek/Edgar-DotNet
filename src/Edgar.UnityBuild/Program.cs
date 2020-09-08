2ced5cef1e4df28144649971fd3af561047af947
Ondřej Nepožitek <ondra@nepozitek.cz>
2020-09-08 19:07:36 +0200

    Add new project for building the Unity version of the library which consists of a single DLL and the Vector2Int class is renamed to EdgarVector2Int to pravent ambiguous references

---
+++ src/Edgar.UnityBuild/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Mono.Cecil;
+
+namespace Edgar.UnityBuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var assemblyName = "EdgarSingleFile.dll";
+            var sourceDir = "EdgarSingleFile";
+            var targetDir = "EdgarSingleFileModified";
+
+            Directory.Delete(targetDir, true);
+            Directory.CreateDirectory(targetDir);
+
+            CopyFilesRecursively(new DirectoryInfo(sourceDir), new DirectoryInfo(targetDir));
+
+            var moduleDefinition = ModuleDefinition.ReadModule(Path.Combine(sourceDir, assemblyName));
+            var vectorType = moduleDefinition.Types.Single(x => x.Name == "Vector2Int");
+            vectorType.Name = "EdgarVector2Int";
+
+            var stream = new FileStream(Path.Combine(targetDir, assemblyName), FileMode.Create);
+            moduleDefinition.Write(stream);
+        }
+
+        public static void CopyFilesRecursively(DirectoryInfo source, DirectoryInfo target) {
+            foreach (DirectoryInfo dir in source.GetDirectories())
+                CopyFilesRecursively(dir, target.CreateSubdirectory(dir.Name));
+            foreach (FileInfo file in source.GetFiles())
+                file.CopyTo(Path.Combine(target.FullName, file.Name));
+        }
+    }
+}
