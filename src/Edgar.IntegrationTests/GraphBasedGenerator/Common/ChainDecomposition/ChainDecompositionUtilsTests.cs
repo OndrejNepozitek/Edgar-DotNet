4bb7521b3f3fa69f47594e9400873cc33b68c3cc
Ondřej Nepožitek <ondra@nepozitek.cz>
2021-05-15 16:48:01 +0200

    Add ChainDecompositionUtils

---
+++ src/Edgar.IntegrationTests/GraphBasedGenerator/Common/ChainDecomposition/ChainDecompositionUtilsTests.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Edgar.GraphBasedGenerator.Common.ChainDecomposition;
+using Edgar.Graphs;
+using Edgar.Legacy.GeneralAlgorithms.Algorithms.Common;
+using NUnit.Framework;
+
+namespace Edgar.IntegrationTests.GraphBasedGenerator.Common.ChainDecomposition
+{
+    [TestFixture]
+    public class ChainDecompositionUtilsTests
+    {
+        [Test]
+        public void MakeBorderNodesWalkable_Path()
+        {
+            var graph = new UndirectedAdjacencyListGraph<int>();
+            var isWalkable = new Dictionary<int, bool>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                graph.AddVertex(i);
+                isWalkable[i] = false;
+
+                if (i > 0)
+                {
+                    graph.AddEdge(i - 1, i);
+                }
+            }
+
+            isWalkable[5] = true;
+            isWalkable[6] = true;
+
+            var expectedResult = new List<int>() {4, 5, 6, 7};
+            var result = ChainDecompositionUtils.MakeBorderNodesWalkable(graph, isWalkable);
+            var resultWalkable = result.Where(x => x.Value).Select(x => x.Key).ToList();
+
+            Assert.That(resultWalkable.SequenceEqualWithoutOrder(expectedResult));
+        }
+
+        [Test]
+        public void GetWalkableComponents_Path()
+        {
+            var graph = new UndirectedAdjacencyListGraph<int>();
+            var isWalkable = new Dictionary<int, bool>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                graph.AddVertex(i);
+                isWalkable[i] = false;
+
+                if (i > 0)
+                {
+                    graph.AddEdge(i - 1, i);
+                }
+            }
+
+            var component1 = new List<int>()
+            {
+                4, 5, 6
+            };
+
+            var component2 = new List<int>()
+            {
+                8
+            };
+
+            foreach (var node in component1.Union(component2))
+            {
+                isWalkable[node] = true;
+            }
+
+
+            var result = ChainDecompositionUtils.GetWalkableComponents(graph, isWalkable);
+
+            Assert.That(result.Count, Is.EqualTo(2));
+            Assert.That(result[0], Is.EquivalentTo(component1));
+            Assert.That(result[1], Is.EquivalentTo(component2));
+        }
+
+        [Test]
+        public void GetReachableNodes_Path()
+        {
+            var graph = new UndirectedAdjacencyListGraph<int>();
+            var isWalkable = new Dictionary<int, bool>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                graph.AddVertex(i);
+                isWalkable[i] = false;
+
+                if (i > 0)
+                {
+                    graph.AddEdge(i - 1, i);
+                }
+            }
+
+            var expectedReachableNodes = new List<int>()
+            {
+                4, 5, 6
+            };
+
+            foreach (var node in expectedReachableNodes)
+            {
+                isWalkable[node] = true;
+            }
+
+            isWalkable[9] = true;
+
+            var reachableNodes = ChainDecompositionUtils.GetReachableNodes(graph, 4, isWalkable);
+
+            Assert.That(reachableNodes.SequenceEqualWithoutOrder(expectedReachableNodes));
+        }
+
+        [Test]
+        public void GetReachableNodes_NotWalkableStart()
+        {
+            var graph = new UndirectedAdjacencyListGraph<int>();
+            var isWalkable = new Dictionary<int, bool>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                graph.AddVertex(i);
+                isWalkable[i] = true;
+
+                if (i > 0)
+                {
+                    graph.AddEdge(i - 1, i);
+                }
+            }
+
+            isWalkable[5] = false;
+
+            var reachableNodes = ChainDecompositionUtils.GetReachableNodes(graph, 5, isWalkable);
+
+            Assert.That(reachableNodes, Is.Empty);
+        }
+    }
+}
---
+++ src/Edgar/GraphBasedGenerator/Common/ChainDecomposition/ChainDecompositionUtils.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Edgar.Graphs;
+
+namespace Edgar.GraphBasedGenerator.Common.ChainDecomposition
+{
+    public static class ChainDecompositionUtils
+    {
+        public static Dictionary<TNode, bool> MakeBorderNodesWalkable<TNode>(IGraph<TNode> graph, Dictionary<TNode, bool> isWalkable)
+        {
+            var isWalkableNew = new Dictionary<TNode, bool>(isWalkable);
+
+            foreach (var node in graph.Vertices)
+            {
+                if (isWalkable[node] == false && graph.GetNeighbors(node).Any(x => isWalkable[x]))
+                {
+                    isWalkableNew[node] = true;
+                }
+            }
+
+            return isWalkableNew;
+        }
+
+        public static List<List<TNode>> GetWalkableComponents<TNode>(IGraph<TNode> graph, Dictionary<TNode, bool> isWalkable)
+        {
+            var visited = new HashSet<TNode>();
+            var components = new List<List<TNode>>();
+
+            foreach (var node in graph.Vertices)
+            {
+                if (visited.Contains(node) == false)
+                {
+                    var component = GetReachableNodes(graph, node, isWalkable);
+
+                    if (component.Count != 0)
+                    {
+                        components.Add(component);
+                        component.ForEach(x => visited.Add(x));
+                    }
+                }
+            }
+
+            return components;
+        }
+
+        public static List<TNode> GetReachableNodes<TNode>(IGraph<TNode> graph, TNode startNode, Dictionary<TNode, bool> isWalkable)
+        {
+            if (!isWalkable[startNode])
+            {
+                return new List<TNode>();
+            }
+
+            var visited = new HashSet<TNode>();
+            var queue = new Queue<TNode>();
+            queue.Enqueue(startNode);
+            visited.Add(startNode);
+
+            while (queue.Count != 0)
+            {
+                var node = queue.Dequeue();
+
+                foreach (var neighbor in graph.GetNeighbors(node))
+                {
+                    if (visited.Contains(neighbor))
+                    {
+                        continue;
+                    }
+
+                    if (!isWalkable[neighbor])
+                    {
+                        continue;
+                    }
+
+                    visited.Add(neighbor);
+                    queue.Enqueue(neighbor);
+                }
+            }
+
+            return visited.ToList();
+        }
+    }
+}
